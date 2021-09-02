fd067509a117eaa893e372002a37646238fcd55b
manoj <manojsharma.mha@gmail.com>
2021-09-03 03:47:08 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+import TextForm from './components/TextForm';
+
 function App() {
+  const [mode, setMode] = useState('light') //whether darkMode is ena
+  const toggleMode = () => {
+    if (mode === 'light'){
+    setMode('dark')
+    document.body.style.backgroundColor='grey'
+    }
+  else {
+  setMode('light')
+  document.body.style.backgroundColor='white'
+}}
   return (
     <>
 
 {/* <Navbar title="TextUtils" AboutText="About TextUtils" /> */}
-<Navbar title="TextUtils" />
+<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
 <div className="container my-3">
-{/* <TextForm heading="Enter the text to analyze"/> */}
-<About />
+<TextForm heading="Enter the text to analyze"/>
+{/* <About /> */}
 </div>
 
     </> 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,18 +30,18 @@ export default function TextForm(props) {
     // setText("new text") //correct way
     return (
         <>
-        <div className="container" >
+        <div className="container" style={{color: props.mode==='dark'?'white':'black'}} >
         
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handUpCase}>Convert to Upper Case</button>
         <button className="btn btn-primary mx-1" onClick={handLoCase}>Convert to Lower Case</button>
         <button className="btn btn-primary mx-1" onClick={handClear}>Clear</button>
         <button className="btn btn-primary mx-1" onClick={handRev}>Reverse Text</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode==='dark'?'grey':'white'}}>
         <h1>Your text summary</h1>
         {text.split(' ').length} words and {text.length} characters
         <p>That is {0.008 * text.split(' ').length} minutes to read</p>
